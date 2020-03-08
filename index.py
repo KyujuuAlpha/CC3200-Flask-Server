85433d3d4ef486d6b0febb6dc0332f048efafde0
KyujuuAlpha <troiryan@att.net>
2020-03-08 01:00:08 -0800

    make this into a post request instead

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from pacman import change_shadow_value
 
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -11,9 +12,10 @@ app.config.from_object('config')
 def index():
     return render_template("index.html")
 
-@app.route('/enemy/<direction>')
-def enemyDirection(direction):
-    change_shadow_value(direction)
+@app.route('/enemy', methods=['POST'])
+def enemy():
+    if request.method == 'POST':
+        change_shadow_value(request.form['direction'])
     return ("nothing")
 
 connect()
